1e6c45313a774a44f87be832edf08650e3229232
airton47 <3002614110101@ingenieria.usac.edu.gt>
2020-09-29 18:58:04 -0600

    carga masiva y definicion de entidades en db

--- [MIA]Consultas.sql
+++ [MIA]Consultas.sql
@@ -1,2 +1,54 @@
 #script con las consultas solicitadas
 
+/*CONSULTA_1:
+Mostrar el nombre del proveedor, número de teléfono, número de orden,
+total de la orden por la cual se haya pagado la mayor cantidad de dinero.
+*/
+
+/*CONSULTA_2:
+Mostrar el número de cliente, nombre, apellido y total del cliente que más
+productos ha comprado.
+*/
+
+/*CONSULTA_3
+Mostrar l a dirección, región, ciudad y código postal hacia l a cual se han hecho
+más solicitudes de pedidos y a cuál menos (en una sola consulta).
+*/
+
+/*CONSULTA_4
+Mostrar el número de cliente, nombre, apellido, el número de órdenes que ha
+realizado y el total de cada uno de l os cinco clientes que más han comprado
+productos de la categoría ‘Cheese’.
+*/
+
+/*CONSULTA_5
+Mostrar el número de mes de l a fecha de registro, nombre y apellido de todos
+los clientes que más han comprado y l os que menos han comprado (en
+dinero) utilizando una sola consulta.
+*/
+
+/*CONSULTA_6
+Mostrar el nombre de l a categoría más y menos vendida y el total vendido en
+dinero (en una sola consulta).
+*/
+
+/*CONSULTA_7
+Mostrar el top 5 de proveedores que más productos han vendido (en dinero)
+de la categoría de productos ‘Fresh Vegetables’.
+*/
+
+/*CONSULTA_8
+Mostrar l a dirección, región, ciudad y código postal de l os clientes que más
+han comprado y de los que menos (en dinero) en una sola consulta.
+*/
+
+/*CONSULTA_9
+Mostrar el nombre del proveedor, número de teléfono, número de orden,
+total de la orden por la cual se haya obtenido la menor cantidad de producto.
+*/
+
+/*CONSULTA_10
+Mostrar el top 10 de l os clientes que más productos han comprado de l a
+categoría ‘Seafood’.
+*/
+
--- [MIA]InstruccionesDDL.sql
+++ [MIA]InstruccionesDDL.sql
@@ -6,19 +6,44 @@ USE DATABASE P1_MIA_2S2020;
 
 
 
-CREATE TABLE CLIENTE();
+CREATE TABLE CLIENTE(
+	id_cliente int not null,
+	nombre varchar(100),
+	telefono varchar(100),
+);
 
 CREATE TABLE COMPANIA();
 
-CREATE TABLE RESION();
+CREATE TABLE REGION();
 
-CREATE TABLE PRODUCTO();
+CREATE TABLE CATEGORIA();
+
+CREATE TABLE PRODUCTO(
+	id_producto int not null,
+	producto varchar(100),
+	precio_unitario float,
+	CONSTRAINT PK_PRODUCTO PRIMARY KEY (id_producto)
+
+);
+
+CREATE TABLE PEDIDO(
+	id_pedido int not null,
+	id_producto int not null,
+	id_cliente int not null,
+	id_compania int not null,
+	cantidad int not null
+	CONSTRAINT PK_PEDIDO PRIMARY KEY (id_pedido),
+	CONSTRAINT FK_idprocuto FOREIGN KEY (id_producto) REFERENCES PRODUCTO(id_producto),
+	CONSTRAINT FK_idcliente FOREIGN KEY (id_cliente) REFERENCES CLIENTE(id_cliente),
+	CONSTRAINT FK_idcompania FOREIGN KEY (id_compania) REFERENCES COMPANIA(id_compania)
+);
 
 CREATE TABLE TEMPORAL(
 	nombre_compania varchar(100),
 	contacto_compania varchar(100),
 	correo_compania varchar(100),
 	telefono_compania varchar(100),
+	tipo char,
 	nombre varchar(100),
 	correo varchar(100),
 	telefono varchar(100),
