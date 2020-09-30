683b6551073e09a3c739faf98f6d6c2aa4b20c95
airton47 <3002614110101@ingenieria.usac.edu.gt>
2020-09-30 08:44:24 -0600

    llenado de data en las tablas

--- [MIA]CargaDeDatos.sql
+++ [MIA]CargaDeDatos.sql
@@ -11,3 +11,10 @@ IGNORE 1 LINES
 	tipo,nombre,correo,telefono,@var1,direccion,ciudad,codigo_postal,region,
 	producto,categoria_producto,cantidad,precio_unitario)
 set fecha_registro = STR_TO_DATE (@var1, '%d/%m/%Y');
+
+/*DEFINICION DE REGIONES*/
+INSERT INTO REGION (region) (SELECT DISTINCT region FROM TEMPORAL);
+
+/*DEFINICION DE CIUDADES*/
+INSERT INTO CIUDAD (ciudad,id_region) (SELECT DISTINCT ciudad,REGION.id_region FROM TEMPORAL,REGION WHERE TEMPORAL.region = REGION.region);
+
--- [MIA]InstruccionesDDL.sql
+++ [MIA]InstruccionesDDL.sql
@@ -6,35 +6,54 @@ USE DATABASE P1_MIA_2S2020;
 
 
 
-CREATE TABLE CLIENTE(
-	id_cliente int not null,
+CREATE TABLE PERSONA(
+	id_persona int not null,
 	nombre varchar(100),
 	telefono varchar(100),
+	PRIMARY KEY (id_)
+);
+
+
+CREATE TABLE REGION(
+	id_region int not null AUTO_INCREMENT,
+	region varchar(100),
+	PRIMARY KEY (id_region)
+);
+
+CREATE TABLE CIUDAD(
+	id_ciudad int not null AUTO_INCREMENT,
+	ciudad varchar(100),
+	id_region int,
+	CONSTRAINT PK_CIUDAD PRIMARY KEY (id_ciudad),
+	CONSTRAINT FK_idregion_ciudad FOREIGN KEY (id_region) REFERENCES REGION(id_region)
 );
 
 CREATE TABLE COMPANIA();
 
-CREATE TABLE REGION();
-
-CREATE TABLE CATEGORIA();
+CREATE TABLE CATEGORIA(
+	id_categoria not null AUTO_INCREMENT,
+	categoria varchar(100),
+	CONSTRAINT PK_CATEGORIA PRIMARY KEY (id_categoria)
+);
 
 CREATE TABLE PRODUCTO(
 	id_producto int not null,
 	producto varchar(100),
+	id_categoria varchar(100),
 	precio_unitario float,
-	CONSTRAINT PK_PRODUCTO PRIMARY KEY (id_producto)
-
+	CONSTRAINT PK_PRODUCTO PRIMARY KEY (id_producto),
+	CONSTRAINT FK_idcategoria_producto FOREIGN KEY (id_categoria) REFERENCES CATEGORIA(id_categoria)
 );
 
 CREATE TABLE PEDIDO(
 	id_pedido int not null,
 	id_producto int not null,
-	id_cliente int not null,
+	id_persona int not null,
 	id_compania int not null,
 	cantidad int not null
 	CONSTRAINT PK_PEDIDO PRIMARY KEY (id_pedido),
 	CONSTRAINT FK_idprocuto FOREIGN KEY (id_producto) REFERENCES PRODUCTO(id_producto),
-	CONSTRAINT FK_idcliente FOREIGN KEY (id_cliente) REFERENCES CLIENTE(id_cliente),
+	CONSTRAINT FK_idcliente FOREIGN KEY (id_persona) REFERENCES CLIENTE(id_persona),
 	CONSTRAINT FK_idcompania FOREIGN KEY (id_compania) REFERENCES COMPANIA(id_compania)
 );
 
@@ -57,4 +76,5 @@ CREATE TABLE TEMPORAL(
 	cantidad int,
 	precio_unitario float
 );
+
 
