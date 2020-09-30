fe7078298e1b539b1f4046ddb126aa63988e41f0
airton47 <3002614110101@ingenieria.usac.edu.gt>
2020-09-29 20:28:51 -0600

    carga masiva en workbench completa

--- [MIA]CargaDeDatos.sql
+++ [MIA]CargaDeDatos.sql
@@ -1,1 +1,13 @@
 #script con instrucciones para la carga de datos
+
+LOAD DATA
+LOCAL INFILE '/home/yelstin/Escritorio/USAC_REPS/MIA_2S2020_PRACTICA1/DataCenterData.csv'
+INTO TABLE TEMPORAL
+CHARACTER SET latin1
+FIELDS TERMINATED BY ';'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(nombre_compania,contacto_compania,correo_compania,telefono_compania,
+	tipo,nombre,correo,telefono,@var1,direccion,ciudad,codigo_postal,region,
+	producto,categoria_producto,cantidad,precio_unitario)
+set fecha_registro = STR_TO_DATE (@var1, '%d/%m/%Y');
