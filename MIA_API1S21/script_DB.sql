24205002abbe5674aee8350a099c1856314de367
airton47 <3002614110101@ingenieria.usac.edu.gt>
2021-04-01 00:22:09 -0600

    se han cargados mas tablas con datos de la tabla temporal:tp_victima, tratamiento, persona_asociada, estado_victima, etc

--- MIA_API1S21/script_DB.sql
+++ MIA_API1S21/script_DB.sql
@@ -42,6 +42,13 @@ CREATE TABLE HOSPITAL(
     CONSTRAINT fk_ubicacion_hospital FOREIGN KEY (cod_ubicacion) REFERENCES UBICACION (id_ubicacion)
 );
 
+CREATE TABLE TRATAMIENTO(
+    id_tratamiento int not null AUTO_INCREMENT,
+    tratamiento varchar(100),
+    efectividad int,
+    CONSTRAINT pk_tratamiento PRIMARY KEY (id_tratamiento)
+);
+
 CREATE TABLE TIPO_CONTACTO (
     id_tipoContacto int not null AUTO_INCREMENT,
     tipoContacto varchar(45),
@@ -49,16 +56,26 @@ CREATE TABLE TIPO_CONTACTO (
 );
 
 CREATE TABLE ESTADO_VICTIMA (
-    id_estado int not null AUTO_INCREMENT,
-    estado varchar(45),
-    CONSTRAINT pk_estado PRIMARY KEY (id_estado)
+    id_estadoVictima int not null AUTO_INCREMENT,
+    estadoVictima varchar(45),
+    CONSTRAINT pk_estado PRIMARY KEY (id_estadoVictima)
 );
 
 CREATE TABLE PERSONA_ASOCIADA (
     id_personaAsociada int not null AUTO_INCREMENT,
-    nombre varchar(45),
-    apellido varchar(45),
+    nombreAsociado varchar(45),
+    apellidoAsociado varchar(45),
     CONSTRAINT pk_personaAsociada PRIMARY KEY (id_personaAsociada)
+);
+
+#TABLA CON REGISTRO DE CONTACTO ENTRE ASOCIACO Y VICTIMA
+CREATE TABLE CONTACTO (
+    cod_victima int not null,
+    cod_asociado int not null,
+    fecha_contacto datetime,
+    CONSTRAINT pk_contacto PRIMARY KEY (cod_victima,cod_asociado)
+    CONSTRAINT fk_victima_contacto FOREIGN KEY (cod_victima) REFERENCES VICTIMA (id_victima),
+    CONSTRAINT fk_personaAsociada_contacto FOREIGN KEY (cod_asociado) REFERENCES ASOCIACO (id_personaAsociada)
 );
 
 CREATE TABLE VICTIMA (
@@ -68,8 +85,10 @@ CREATE TABLE VICTIMA (
     cod_estado int,
     fecha_registro datetime,
     facha_muerte datetime,
+    cod_hospital int,
     CONSTRAINT pk_victima PRIMARY KEY (id_victima),
-    CONSTRAINT fk_estado_victima PRIMARY KEY (cod_estado) REFERENCES ESTADO_VICTIMA (id_estado)
+    CONSTRAINT fk_estado_victima FOREIGN KEY (cod_estado) REFERENCES ESTADO_VICTIMA (id_estadoVictima)
+     CONSTRAINT fk_hospital_victaima FOREIGN KEY (cod_hospital) REFERENCES HOSPITAL (id_hospital)
 );
 
 CREATE TABLE DETALLE_VICTIMA_ASOCIADO (
@@ -84,12 +103,6 @@ CREATE TABLE DETALLE_VICTIMA_ASOCIADO (
     CONSTRAINT fk_tipoContacto_detalleVictimaAsociada FOREIGN KEY (cod_tipoContacto) REFERENCES TIPO_CONTACTO (id_tipoContacto)
 );
 
-CREATE TABLE TRATAMIENTO(
-    id_tratamiento int not null AUTO_INCREMENT,
-    tratamiento varchar(100),
-    efectividad int,
-    CONSTRAINT pk_tratamiento PRIMARY KEY (id_tratamiento)
-);
 
 
 
